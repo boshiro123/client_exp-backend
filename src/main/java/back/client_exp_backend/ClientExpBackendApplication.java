92a620537dd27698cfea10bd273045f1a6ab3c58
boshiro123 <b.shiroki@mail.ru>
2025-04-04 16:01:32 +0300

    first commit

---
+++ src/main/java/back/client_exp_backend/ClientExpBackendApplication.java
@@ -0,0 +1,13 @@
+package back.client_exp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientExpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientExpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/back/client_exp_backend/ClientExpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package back.client_exp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientExpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
